0232f73f27ab4d28ffe8d615b600a61f2e2f8bf5
zylladys <gfernandes1990@gmail.com>
2026-05-21 23:58:40 -0300

    Render + Supabase Setup

--- app/database.py
+++ app/database.py
@@ -1,11 +1,18 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./data/database.db"
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import (
+    sessionmaker,
+    declarative_base
+)
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
