4a44c89da20cbdbe33786b42b5d8bee30416edbe
zylladys <gfernandes1990@gmail.com>
2026-05-22 09:31:12 -0300

    Enable CORS

--- app/main.py
+++ app/main.py
@@ -24,7 +24,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000",
+        "https://pxy-hub.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
