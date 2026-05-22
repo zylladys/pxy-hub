380d855cd80d5634c1057d395b4e2cfef67e24f5
zylladys <gfernandes1990@gmail.com>
2026-05-22 01:23:15 -0300

    Fix Render Deploy

--- app/database.py
+++ app/database.py
@@ -1,12 +1,27 @@
+import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./data/database.db"
+from sqlalchemy.orm import (
+    sessionmaker,
+    declarative_base
+)
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+if DATABASE_URL.startswith(
+    "postgres://"
+):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql+psycopg2://",
+        1
+    )
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
